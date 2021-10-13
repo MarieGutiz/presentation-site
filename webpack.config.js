2a5c48a6b30fc2cab997f244f2ef750bd4fec32c
Marie <mariela.gutierrez2906@gmail.com>
2021-10-14 00:27:34 +0300

    Complete building process config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
 
 const postCSSPlugins = [
@@ -10,31 +16,22 @@ const postCSSPlugins = [
     require('autoprefixer')
   ]
 
+  class Compile{
+    apply(compiler) {
+      compiler.hooks.done.tap('Copy images', function() {
+        fse.copySync('./app/assets/images', './docs/assets/images')
+      })
+    }
+  }
   let cssConfig = {
       test: /\.css$/i,
-      use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+      use: ["css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
           
   }
-  let svgConfig= {
-    test: require.resolve('snapsvg/dist/snap.svg.js'),
-    use: 'imports-loader?this=>window,fix=>module.exports=0',
-  }
-  module.exports = {
+
+  let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
-      filename: 'bundled.js',
-      path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
-      before: function(app, server) {
-        server._watch('./app/**/*.html')
-      },
-      contentBase: path.join(__dirname, 'app'),
-      hot: true,
-      port: 3000,
-      host: '0.0.0.0'
-    },
-    mode: 'development',
+    plugins:[new HtmlWebpackPlugin({filename:'index.html', template:'./app/index.html'})],
     module: {
       rules: [
         cssConfig,
@@ -43,10 +40,58 @@ const postCSSPlugins = [
           use: 'imports-loader?wrapper=window&additionalCode=module.exports=0;',
         },
       ]
-    },
+    } ,
     resolve: {
       alias: {
         snapsvg: 'snapsvg/dist/snap.svg.js',
       }
     }
   }
+  if(currentTask == 'dev'){
+    cssConfig.use.unshift('style-loader')
+    config.output = {
+      filename: 'bundled.js',
+       path: path.resolve(__dirname, 'app')
+    }
+    config.devServer = {
+      before: function(app, server) {
+        server._watch('./app/**/*.html')
+      },
+      contentBase: path.join(__dirname, 'app'),
+      hot: true,
+      port: 3000,
+      host: '0.0.0.0'
+    }
+    config.mode ='development'
+  }
+  if(currentTask == 'build'){
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    })
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+    config.output={
+       filename: '[name].[chunkhash].js',
+       chunkFilename: '[name].[chunkhash].js',
+       path: path.resolve(__dirname, 'docs')
+    }
+    config.mode = 'production'
+    config.optimization = {
+      splitChunks: {chunks: 'all',minSize: 1000},
+      minimize :true,
+      minimizer: [`...`, new CssMinimizerPlugin()]
+    }
+    config.plugins.push(new CleanWebpackPlugin(), 
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    new Compile()
+    )
+  }
+
+
+module.exports = config
